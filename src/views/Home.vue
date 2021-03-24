fa6c0f99d1ddd666e1cf5ec5e1102885bd6588c2
stephen.brooks220413 <stephen.brooks220413@gmail.com>
2021-03-24 15:34:50 -0700

    update

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,144 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <!-- gameboard -->
+    <div class="container">
+      <ul>
+        <li class="game-card">
+          <!-- code here -->
+          <img
+            src="https://images.unsplash.com/photo-1500061228850-950b0dc792c1?ixid=MXwxMjA3fDB8MHxzZWFyY2h8M3x8Ym9hcmRnYW1lc3xlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+            alt=""
+            class="img-fluid"
+            style="width: 100%"
+          />
+        </li>
+        <li class="game-card">
+          <!-- code here -->
+          <img
+            src="https://images.unsplash.com/photo-1500061228850-950b0dc792c1?ixid=MXwxMjA3fDB8MHxzZWFyY2h8M3x8Ym9hcmRnYW1lc3xlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+            alt=""
+            class="img-fluid"
+            style="width: 100%"
+          />
+        </li>
+        <li class="game-card">
+          <!-- code here -->
+          <img
+            src="https://images.unsplash.com/photo-1500061228850-950b0dc792c1?ixid=MXwxMjA3fDB8MHxzZWFyY2h8M3x8Ym9hcmRnYW1lc3xlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+            alt=""
+            class="img-fluid"
+            style="width: 100%"
+          />
+        </li>
+        <li class="game-card">
+          <!-- code here -->
+          <img
+            src="https://images.unsplash.com/photo-1500061228850-950b0dc792c1?ixid=MXwxMjA3fDB8MHxzZWFyY2h8M3x8Ym9hcmRnYW1lc3xlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+            alt=""
+            class="img-fluid"
+            style="width: 100%"
+          />
+        </li>
+        <li class="game-card">
+          <!-- code here -->
+          <img
+            src="https://images.unsplash.com/photo-1500061228850-950b0dc792c1?ixid=MXwxMjA3fDB8MHxzZWFyY2h8M3x8Ym9hcmRnYW1lc3xlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+            alt=""
+            class="img-fluid"
+            style="width: 100%"
+          />
+        </li>
+        <li class="game-card">
+          <!-- code here -->
+          <img
+            src="https://images.unsplash.com/photo-1500061228850-950b0dc792c1?ixid=MXwxMjA3fDB8MHxzZWFyY2h8M3x8Ym9hcmRnYW1lc3xlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+            alt=""
+            class="img-fluid"
+            style="width: 100%"
+          />
+        </li>
+        <li class="game-card">
+          <!-- code here -->
+          <img
+            src="https://images.unsplash.com/photo-1500061228850-950b0dc792c1?ixid=MXwxMjA3fDB8MHxzZWFyY2h8M3x8Ym9hcmRnYW1lc3xlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+            alt=""
+            class="img-fluid"
+            style="width: 100%"
+          />
+        </li>
+        <li class="game-card-l">
+          <!-- code here -->
+          <img
+            src="https://images.unsplash.com/photo-1500061228850-950b0dc792c1?ixid=MXwxMjA3fDB8MHxzZWFyY2h8M3x8Ym9hcmRnYW1lc3xlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+            alt=""
+            class="img-fluid"
+            style="width: 100%"
+          />
+        </li>
+        <li class="game-card-l">
+          <!-- code here -->
+          <img
+            src="https://images.unsplash.com/photo-1500061228850-950b0dc792c1?ixid=MXwxMjA3fDB8MHxzZWFyY2h8M3x8Ym9hcmRnYW1lc3xlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+            alt=""
+            class="img-fluid"
+            style="width: 100%"
+          />
+        </li>
+        <li class="game-card-l">
+          <!-- code here -->
+          <img
+            src="https://images.unsplash.com/photo-1500061228850-950b0dc792c1?ixid=MXwxMjA3fDB8MHxzZWFyY2h8M3x8Ym9hcmRnYW1lc3xlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+            alt=""
+            class="img-fluid"
+            style="width: 100%"
+          />
+        </li>
+        <li class="game-card-l">
+          <!-- code here -->
+          <img
+            src="https://images.unsplash.com/photo-1500061228850-950b0dc792c1?ixid=MXwxMjA3fDB8MHxzZWFyY2h8M3x8Ym9hcmRnYW1lc3xlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+            alt=""
+            class="img-fluid"
+            style="width: 100%"
+          />
+        </li>
+        <li class="game-card-l">
+          <!-- code here -->
+          <img
+            src="https://images.unsplash.com/photo-1500061228850-950b0dc792c1?ixid=MXwxMjA3fDB8MHxzZWFyY2h8M3x8Ym9hcmRnYW1lc3xlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+            alt=""
+            class="img-fluid"
+            style="width: 100%"
+          />
+        </li>
+        <li class="game-card-l">
+          <!-- code here -->
+          <img
+            src="https://images.unsplash.com/photo-1500061228850-950b0dc792c1?ixid=MXwxMjA3fDB8MHxzZWFyY2h8M3x8Ym9hcmRnYW1lc3xlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+            alt=""
+            class="img-fluid"
+            style="width: 100%"
+          />
+        </li>
+        <li class="game-card-l">
+          <!-- code here -->
+          <img
+            src="https://images.unsplash.com/photo-1500061228850-950b0dc792c1?ixid=MXwxMjA3fDB8MHxzZWFyY2h8M3x8Ym9hcmRnYW1lc3xlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+            alt=""
+            class="img-fluid"
+            style="width: 100%"
+          />
+        </li>
+        <!-------- center board --------->
+        <span class="game-card-c"></span>
+      </ul>
+    </div>
+    <HelloWorld />
+    <!-- <HelloWorld />
+    <HelloWorld />
+    <HelloWorld />
+    <HelloWorld />
+    <HelloWorld /> -->
   </div>
 </template>
 
@@ -16,3 +153,7 @@ import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
 })
 export default class Home extends Vue {}
 </script>
+
+<style lang="scss">
+@import "../assets/helloworld";
+</style>
