a7b56b834c306fdbc312cd35020a122fd0d8ac2c
stephen.brooks220413 <stephen.brooks220413@gmail.com>
2021-03-23 19:25:03 -0700

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -6,25 +6,19 @@
   <router-view />
 </template>
 
+<script>
+import * as mdb from "mdb-ui-kit";
+
+export default {
+  mdb,
+};
+</script>
+
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+@import "~mdb-ui-kit/css/mdb.min.css";
+@import "./assets/navbar.scss";
+@import "./assets/main.scss";
+@import "./assets/board.scss";
+@import "./assets/player.scss";
+@import "./assets/styles.scss";
 </style>
